508d3e62a22d962af5916efc58a85e0f87317872
David Hunter <david.hunter@campspot.com>
2021-04-22 17:25:45 -0600

    more customization

--- assets/js/search.js
+++ assets/js/search.js
@@ -29,16 +29,16 @@ $(document).ready(function() {
       $('.search-form-container').removeClass('is-active');
     });
 
-    search_field.ghostHunter({
-      results: search_results,
-      onKeyUp         : true,
-      rss             : base_url + '/feed.xml',
-      zeroResultsInfo : false,
-      info_template   : "<h4 class='heading'>Number of posts found: {{amount}}</h4>",
-      result_template : search_result_template,
-      before: function() {
-        search_results.fadeIn();
-      }
-    });
+    // search_field.ghostHunter({
+    //   results: search_results,
+    //   onKeyUp         : true,
+    //   rss             : base_url + '/feed.xml',
+    //   zeroResultsInfo : false,
+    //   info_template   : "<h4 class='heading'>Number of posts found: {{amount}}</h4>",
+    //   result_template : search_result_template,
+    //   before: function() {
+    //     search_results.fadeIn();
+    //   }
+    // });
 
   });
